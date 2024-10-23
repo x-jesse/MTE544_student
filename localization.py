1b085177911d2b2191cfaa352f0ecfc03790d1d2
Jesse Xia <68565085+x-jesse@users.noreply.github.com>
2024-10-23 18:37:55 -0400

    Update localization

--- localization.py
+++ localization.py
@@ -19,22 +19,29 @@ class localization(Node):
         # TODO Part 3: Define the QoS profile variable based on whether you are using the simulation (Turtlebot 3 Burger) or the real robot (Turtlebot 4)
         # Remember to define your QoS profile based on the information available in "ros2 topic info /odom --verbose" as explained in Tutorial 3
 
-        odom_qos=...
+        odom_qos=QoSProfile(reliability=2, durability=2, history=1, depth=10)
         
         self.loc_logger=Logger("robot_pose.csv", ["x", "y", "theta", "stamp"])
         self.pose=None
         
         if localizationType == rawSensor:
         # TODO Part 3: subscribe to the position sensor topic (Odometry)
-        # ...
+            self.create_subscription(odom, "/odom", self.odom_callback, qos_profile=odom_qos)
         else:
             print("This type doesn't exist", sys.stderr)
     
     
     def odom_callback(self, pose_msg):
+        # Get timestamp from message (timestamp is always in the message header)
+        timestamp = Time.from_msg(pose_msg.header.stamp).nanoseconds
+
+        # Get message data
+        theta = pose_msg.pose.pose.orientation
+        x_pos = pose_msg.pose.pose.position.x
+        y_pos = pose_msg.pose.pose.position.y
         
         # TODO Part 3: Read x,y, theta, and record the stamp
-        self.pose=[ ... ]
+        self.pose=[ x_pos, y_pos, theta ]
         
         # Log the data
         self.loc_logger.log_values([self.pose[0], self.pose[1], self.pose[2], Time.from_msg(self.pose[3]).nanoseconds])
@@ -45,4 +52,5 @@ class localization(Node):
 # TODO Part 3
 # Here put a guard that makes the node run, ONLY when run as a main thread!
 # This is to make sure this node functions right before using it in decision.py
-    
+if __name__ == '__main__':
+    localizer = localization()
