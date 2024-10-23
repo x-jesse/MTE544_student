d2843e728af140f21df018189268485db6ea32bc
CalebKNg <caleb17ng@gmail.com>
2024-10-23 19:41:39 -0400

    error threshold

--- decisions.py
+++ decisions.py
@@ -30,7 +30,6 @@ class decision_maker(Node):
 
         #TODO Part 4: Create a publisher for the topic responsible for robot's motion
         self.publisher=self.create_publisher(publisher_msg, publishing_topic, qos_publisher)
-
         publishing_period=1/rate
         
         # Instantiate the controller
@@ -62,7 +61,7 @@ class decision_maker(Node):
     def timerCallback(self):
         
         # TODO Part 3: Run the localization node
-        ...    # Remember that this file is already running the decision_maker node.
+        spin_once({self.localizer})   # Remember that this file is already running the decision_maker node.
 
         if self.localizer.getPose()  is  None:
             print("waiting for odom msgs ....")
@@ -71,10 +70,13 @@ class decision_maker(Node):
         vel_msg=Twist()
         
         # TODO Part 3: Check if you reached the goal
+        errorThreshold = 0.1
+        
         if type(self.goal) == list:
-            reached_goal=True
+            
+            reached_goal= calculate_linear_error(self.localizer.getPose(), self.goal) > errorThreshold
         else: 
-            reached_goal=False
+            reached_goal= calculate_linear_error(self.localizer.getPose(), self.goal) > errorThreshold
         
 
         if reached_goal:
@@ -85,17 +87,14 @@ class decision_maker(Node):
             self.controller.PID_linear.logger.save_log()
             
             #TODO Part 3: exit the spin
-            
+            raise SystemExit
         
         velocity, yaw_rate = self.controller.vel_request(self.localizer.getPose(), self.goal, True)
-
+        # fill in vel_msg
+        vel_msg.linear.x = velocity
+        vel_msg.angular.z = yaw_rate
         #TODO Part 4: Publish the velocity to move the robot
-        msg = Twist()
-        msg.linear.x = velocity
-        msg.angular.z = yaw_rate
-
-        self.publisher.publish(msg)
-        
+        self.publisher.publish(vel_msg)
 
 import argparse
 
@@ -112,12 +111,12 @@ def main(args=None):
 
     # TODO Part 4: instantiate the decision_maker with the proper parameters for moving the robot
     if args.motion.lower() == "point":
-        print('point')
-        DM=decision_maker(Twist, '/cmd_vel', odom_qos, [1, 1])
+        DM=decision_maker(Twist, '\cmd_vel', odom_qos, [1, 1], motion_type=POINT_PLANNER)
     elif args.motion.lower() == "trajectory":
-        DM=decision_maker(Twist, '/cmd_vel', odom_qos, [1, 1])
+        DM=decision_maker(Twist, '\cmd_vel', odom_qos, [1, 1], motion_type=TRAJECTORY_PLANNER)
     else:
         print("invalid motion type", file=sys.stderr)        
+    
     
     
     try:
